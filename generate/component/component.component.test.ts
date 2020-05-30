cf0c58b16d324caf160065abff3be8cfdd1d7d50
Patryk Loter <patryk.loter@gmail.com>
2020-05-30 21:01:14 +0100

    add test

--- generate/component/component.component.test.ts
+++ generate/component/component.component.test.ts
@@ -1,2 +1,8 @@
 import { shallowMount } from '@vue/test-utils';
 import {{ name }} from '{{name}}.component.vue'
+
+test('it can be mount', () => {
+  const wrapper = shallowMount({{name}});
+
+  expect(wrapper).toBeInstanceOf(Object);
+});
